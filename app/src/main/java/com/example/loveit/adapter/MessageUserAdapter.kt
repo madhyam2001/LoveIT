03bba965e4896d6376c2c254feef26e513039b24
madhyam2001 <madhyampatra12@gmail.com>
2023-05-09 01:30:15 +0530

    There is a bug in firebase senderD+ReceiverId need to fix that

--- app/src/main/java/com/example/loveit/adapter/MessageUserAdapter.kt
+++ app/src/main/java/com/example/loveit/adapter/MessageUserAdapter.kt
@@ -3,12 +3,17 @@ package com.example.loveit.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.loveit.databinding.UserItemLayoutBinding
 import com.example.loveit.model.UserModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class MessageUserAdapter(val context: Context,val list: ArrayList<UserModel>) :
+class MessageUserAdapter(val context: Context,val list: ArrayList<String>,val chatKey : List<String>) :
     RecyclerView.Adapter<MessageUserAdapter.MessageUserViewHolder>() {
     inner class MessageUserViewHolder(val binding: UserItemLayoutBinding)
         : RecyclerView.ViewHolder(binding.root)
@@ -18,9 +23,29 @@ class MessageUserAdapter(val context: Context,val list: ArrayList<UserModel>) :
     }
 
     override fun onBindViewHolder(holder: MessageUserViewHolder, position: Int) {
-        Glide.with(context).load(list[position].image).into(holder.binding.userImage)
 
-        holder.binding.userName.text=list[position].name
+        FirebaseDatabase.getInstance().getReference("users")
+            .child(list[position]).addValueEventListener(
+                object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()){
+                            val data=snapshot.getValue(UserModel::class.java)
+                            Glide.with(context).load(data!!.image).into(holder.binding.userImage)
+
+                            holder.binding.userName.text=data.name
+
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+            )
+
+
 
     }
 
--- app/src/main/java/com/example/loveit/ui/MessageFragment.kt
+++ app/src/main/java/com/example/loveit/ui/MessageFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.loveit.R
 import com.example.loveit.adapter.MessageUserAdapter
 import com.example.loveit.databinding.FragmentMessageBinding
 import com.example.loveit.ui.DatingFragment.Companion.list
+import com.example.loveit.utils.Config
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MessageFragment : Fragment() {
 
@@ -20,8 +27,44 @@ class MessageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentMessageBinding.inflate(layoutInflater)
-        binding.recyclerView.adapter=MessageUserAdapter(requireContext(),list!!)
+
+        getdata()
+
         return binding.root
+    }
+
+    private fun getdata() {
+        Config.showDialog(requireContext())
+        var list= arrayListOf<String>()
+        var newList= arrayListOf<String>()
+        val currentId= FirebaseAuth.getInstance()?.currentUser!!.uid
+        FirebaseDatabase.getInstance().getReference("chats")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    for (data in snapshot.children)
+                    {
+                        if(data.key!!.contains(currentId))
+                        {
+                            list.add(data.key!!.replace(currentId!!,""))
+                            newList.add(data.key!!)
+                        }
+
+                    }
+
+                    binding.recyclerView.adapter=MessageUserAdapter(requireContext(), list,newList)
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(requireContext(),error.message,Toast.LENGTH_SHORT).show()
+
+                Config.hideDialog()
+
+                }
+
+            })
     }
 
 
